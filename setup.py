f1f1426f6516a2823d6051f74228b1371ae4b449
Ji Zhang <zhangji0218@163.com>
2019-03-23 00:22:17 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="datacleanbot",
-    version="0.1",
+    version="0.2",
     author="Ji Zhang",
     author_email="",
     description="automated data cleaning tool",
