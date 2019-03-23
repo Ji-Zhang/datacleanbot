87a2ce76fe5adda954cd5ac209262635ec4537fd
Ji Zhang <zhangji0218@163.com>
2019-03-23 16:10:59 +0100

    update doc

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -171,5 +171,5 @@ texinfo_documents = [
      'Miscellaneous'),
 ]
 
-autodoc_mock_imports = ['datacleanbot', 'pystruct', 'pymc3', 'pyro-ppl', 'rpy2']
+autodoc_mock_imports = ['datacleanbot', 'ipython', 'pystruct', 'pymc3', 'pyro-ppl', 'rpy2']
 
